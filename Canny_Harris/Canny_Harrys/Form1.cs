2fb5217633453483387efbfac8afdd73e5176cba
goncalo.mendes8 <gnaomne@gmail.com>
2019-03-29 20:11:45 -0400

    Computer-Vision

---
+++ Canny_Harris/Canny_Harrys/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Canny_Harrys
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap bm;
+        Image<Gray, Byte> img;
+        Mat resImage = new Mat();
+        Image<Gray, float> harrisImage = null;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CvInvoke.Canny(img, resImage, 50, 200);
+            picProcImage.BackgroundImage = resImage.Bitmap;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "jpeg files (*.jpg)|*.jpg|(*.gif)|gif||";
+            if (DialogResult.OK == dialog.ShowDialog())
+            {
+
+                img = new Image<Gray, Byte>(dialog.FileName);
+                picOrigImage.BackgroundImage = img.ToBitmap();
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+                harrisImage = new Image<Gray, float>(img.Size);
+                CvInvoke.CornerHarris(img, harrisImage, 2, 3, 0.01);
+                picProcImage.BackgroundImage = harrisImage.ToBitmap();
+           
+        }
+    }
+}
---
+++ Canny_Harris/PythonApplication5/Canny_Harris_Python.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+#Load a color image in grayscale
+img = cv2.imread('C:/Users/Windows User/Desktop/UB Spring 2019/Computer Vision/Assignment 2/Sample_Image.png',1) # 0 for gray, 1 for color
+cv2.imshow('image',img)
+#---------------canny edge detection-------------
+img = cv2.imread('C:/Users/Windows User/Desktop/UB Spring 2019/Computer Vision/Assignment 2/Sample_Image.png',0)
+edges = cv2.Canny(img,50,200)
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+plt.show()
+
+#-------------------------------------------------
+#--------------------Harris Corner Detection------
+filename = 'C:/Users/Windows User/Desktop/UB Spring 2019/Computer Vision/Assignment 2/CPEG540_tutorials/CPEG-540_tutorials/profilepic.png'
+img = cv2.imread(filename)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# find Harris corners
+gray = np.float32(gray)
+dst = cv2.cornerHarris(gray,2,3,0.04)
+#result is dilated for marking the corners, not important
+dst = cv2.dilate(dst,None)
+# Threshold for an optimal value, it may vary depending on the image.
+img[dst>0.01*dst.max()]=[0,0,255]
+cv2.imshow('dst',img)
+#-------------------------------------------------
+cv2.waitKey(0)
+cv2.destroyAllWindows()
